00fc546260b14abcacd45292a5ae1c4bea2df1b5
unknown <arranwoodruff@gmail.com>
2019-04-10 21:35:09 -0600

    part1 finished

--- src/components/welldata/WellSearch.js
+++ src/components/welldata/WellSearch.js
@@ -1,19 +1,61 @@
-import React, { Component } from "react";
-import WellSearch2 from "./WellSearch2";
+import React, { useState, useEffect } from "react";
+import classnames from "classnames";
 
-class WellSearch extends Component {
-  state = {
-    wellFound: false,
-    selectedWell: {},
-    productionData: [],
-    searchInput: "",
-    location: ""
+export default function WellSearch2() {
+  const [searchValue, setSearchValue] = useState("");
+  const [error, setError] = useState("");
+
+  const [locations, setLocations] = useState([]);
+  const [productions, setProductions] = useState([]);
+  const [matchingProductions, setMatchingProductions] = useState([]);
+
+  useEffect(() => {
+    loadLocations();
+    loadProductions();
+  }, []);
+
+  useEffect(() => {
+    if (locations.includes(searchValue)) {
+      setError("");
+      const matchingProds = [];
+      for (let i = 0; i < productions.length; i++) {
+        if (productions[i].location.trim() === searchValue) {
+          matchingProds.push(productions[i]);
+        }
+      }
+      setMatchingProductions(matchingProds);
+    } else if (searchValue !== "") {
+      setError("Well Not Found.");
+      setMatchingProductions([]);
+    }
+  }, [searchValue]);
+
+  const loadProductions = () => {
+    const xhr = new XMLHttpRequest();
+    const prods = [];
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const xmldoc = xhr.responseText;
+        var XMLParser = require("react-xml-parser");
+        var x = new XMLParser().parseFromString(xmldoc);
+        const records = x.getElementsByTagName("productiondata");
+        for (let i = 0; i < 700; i++) {
+          const obj = {};
+          for (let j = 0; j < records[i].children.length; j++) {
+            obj[records[i].children[j].name] = records[i].children[j].value;
+          }
+          prods.push(obj);
+        }
+      }
+    };
+    setProductions(prods);
+    xhr.open("GET", "res/productiondata.xml", true);
+    xhr.send();
   };
 
-  findWell() {
+  const loadLocations = () => {
     const xhr = new XMLHttpRequest();
-    const searchWell = {};
-    const search = this.state.searchInput;
+    const locs = [];
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
         const xmldoc = xhr.responseText;
@@ -21,70 +63,68 @@ class WellSearch extends Component {
         var x = new XMLParser().parseFromString(xmldoc);
         const records = x.getElementsByTagName("welldata");
         for (let i = 0; i < records.length; i++) {
-          if (records[i].children[0].value.trim() === search) {
-            for (let j = 0; j < records[i].children.length; j++) {
-              searchWell[records[i].children[j].name] =
-                records[i].children[j].value;
-            }
-          }
+          locs.push(records[i].children[0].value.trim());
         }
       }
     };
     xhr.open("GET", "res/welldata.xml", true);
     xhr.send();
-    this.setState({
-      selectedWell: searchWell,
-      wellFound: true,
-      location: searchWell.location
-    });
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    setLocations(locs);
   };
 
-  render() {
-    let searchResult = <span>{this.state.selectedWell.location}</span>;
-    return (
-      <div className="container">
-        <WellSearch2 />
-        <div className="card">
-          <div className="card-header">
-            <h3>Alberta Township Well Search</h3>
-          </div>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-6 offset-3">
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend">
-                    <label className="input-group-text" htmlFor="searchInput">
-                      Well Location
-                    </label>
-                  </div>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="searchInput"
-                    name="searchInput"
-                    value={this.state.searchInput}
-                    onChange={this.onChange}
-                    placeholder="Location..."
-                  />
-                </div>
-                <input
-                  type="button"
-                  className="btn btn-primary form-control"
-                  value="Search"
-                  onClick={this.findWell.bind(this)}
-                />
-              </div>
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-header">
+          <h3>Alberta Township Well Search</h3>
+        </div>
+        <div className="card-body">
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <label
+                className={classnames("input-group-text", {
+                  "text-danger border-danger": error
+                })}
+                htmlFor="search"
+              >
+                Well Location
+              </label>
             </div>
-            {this.state.wellFound ? this.state.location : ""}
+            <input
+              className={classnames("form-control", { "is-invalid": error })}
+              type="text"
+              id="search"
+              value={searchValue}
+              onChange={e => setSearchValue(e.target.value)}
+            />
           </div>
+          <h3 className="text-danger text-center">
+            {error !== "" ? error : ""}
+          </h3>
+          {matchingProductions ? (
+            <table style={{ tableLayout: "fixed" }} className="table">
+              <tbody>
+                <tr>
+                  <th>Location</th>
+                  <th>Date</th>
+                  <th>Oil Production</th>
+                  <th>Water Production</th>
+                  <th>Gas Production</th>
+                </tr>
+                {matchingProductions.map(production => (
+                  <tr>
+                    <td>{production.location}</td>
+                    <td>{production.date}</td>
+                    <td>{production.oilproduction}</td>
+                    <td>{production.waterproduction}</td>
+                    <td>{production.gasproduction}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : null}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default WellSearch;
