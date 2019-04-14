760a6264592cf9517ffad55482cdbbf1401313d3
yaehjs <arranwoodruff@gmail.com>
2019-04-14 12:25:48 -0600

    Updates

---
+++ precache-manifest.b92b0a6a1b2c2f4bb9997d24b6496052.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "373e77aadae2d7becd25",
+    "url": "/256-finalproject/static/js/runtime~main.373e77aa.js"
+  },
+  {
+    "revision": "b02b9b4ff9e6bd49ffb4",
+    "url": "/256-finalproject/static/js/main.b02b9b4f.chunk.js"
+  },
+  {
+    "revision": "c84cb7cd7dec3ca8069f",
+    "url": "/256-finalproject/static/js/1.c84cb7cd.chunk.js"
+  },
+  {
+    "revision": "b02b9b4ff9e6bd49ffb4",
+    "url": "/256-finalproject/static/css/main.b2d01ea2.chunk.css"
+  },
+  {
+    "revision": "480e390b2c69fadc239bbfc87b70da9b",
+    "url": "/256-finalproject/index.html"
+  }
+];
